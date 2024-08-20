1a363d394e44f3328db9b9fbf355b76afe14856f
vicmmoraM <vicmmora@espol.edu.ec>
2024-08-20 18:32:08 -0500

    Datos

---
+++ Cifrado.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import simpledialog
+root = tk.Tk
+FraseOriginal = simpledialog.askstring("Ingreso de datos", "Ingrese la frase que desea citar")
+root.withdraw() #Ocultar ventana
+
